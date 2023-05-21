a920deea15d4ff5e07436a4879c20d2b12a279c7
jintao_xiao <xiaojintao@outlook.com>
2023-05-21 19:49:45 +0800

    update

--- cats-effect/src/main/scala/jt/Resources.scala
+++ cats-effect/src/main/scala/jt/Resources.scala
@@ -7,6 +7,7 @@ import utils._
 import java.util.Scanner
 import java.io.FileReader
 import java.io.File
+import cats.effect.kernel.Resource
 
 object Resources extends IOApp.Simple {
 
@@ -45,9 +46,43 @@ object Resources extends IOApp.Simple {
   def bracketReadFile(path: String): IO[Unit] = 
     openFileScanner(path).bracket{ scanner => readLineByLine(scanner) } { scanner => IO{ scanner.close() }} 
 
+  def connFromConfig(path: String): IO[Unit] = 
+    openFileScanner(path)
+      .bracket{ scanner => 
+        IO(new Connection(scanner.nextLine())).bracket{ 
+          conn => conn.open.myDebug >> IO.never
+        }(conn => conn.close.myDebug.void)  
+       } ( scanner => IO("closing file").myDebug >> IO(scanner.close()))
+
+  val connectionResource = Resource.make(IO(new Connection("rockthejvm.com")))(_.close.myDebug.void)
+
+  val resourceFetchUrl = for {
+    fib <- connectionResource.use(conn => conn.open >> IO.never).start
+    _ <- IO.sleep(1.second) >> fib.cancel
+  } yield ()
+
+  val simpleResource = IO("some resource")
+  val usingResource: String => IO[String] = str => IO(s"using the string $str").myDebug
+  val releaseResource: String => IO[Unit] = str => IO(s"finalizing the str $str").myDebug.void
+
+  val usingResourceWithBracket = simpleResource.bracket(usingResource)(releaseResource)
+  val usingResourceWithResource = Resource.make(simpleResource)(releaseResource).use(usingResource)
+
+  def resourceReadFile(path: String): IO[Unit] = 
+    IO(s"opening file at $path") >>  
+      Resource.make(openFileScanner(path))(scanner => IO(scanner.close).void).use(readLineByLine)    
+ 
+  def cancelReadFile(path: String) = for {
+    fib <- resourceReadFile(path).start  
+    _ <- IO.sleep(2.seconds) >> fib.cancel
+  } yield ()
+
+  
+
   override def run: IO[Unit] = 
     // asyncFetchUrl.void    
     // correctAsyncFetchUrl.void
     // bracketProgram
-    bracketReadFile("./.gitignore")
+    // bracketReadFile("./.gitignore")
+    resourceFetchUrl
 }
